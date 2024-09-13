a13bb922ff4a6f3a4bbd47027fbcc8ad983a681e
akhilesh731 <akhil.kumar9088@gmail.com>
2024-09-13 16:43:42 +0530

    MyFirstWebApp updated!

--- MyFirstWebApp/src/main/java/com/springboot/webapp/todo/TodoController.java
+++ MyFirstWebApp/src/main/java/com/springboot/webapp/todo/TodoController.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TodoController {
@@ -31,15 +34,29 @@ public class TodoController {
 	}
 	
 	@GetMapping("add-todo")
-	public String showNewTodoPage() {
+	public String showNewTodoPage(ModelMap model) {
 		System.out.println("showNewTodoPage_");
+		Todo todo = new Todo();
+		model.put("todo", todo);
 		return "todo";
 	}
 	
 	@PostMapping("add-todo")
-	public String addNewTodo(@RequestParam String description, ModelMap modelMap) {
+	public String addNewTodo(ModelMap modelMap, @Valid Todo todo, BindingResult result) {
 		System.out.println("addNewTodo_");
-		todoService.addTodo((String)modelMap.get("name"), description, LocalDate.now().plusMonths(11), false);
+		if(result.hasErrors()) {
+			System.out.println(result);
+			return "todo";
+		}
+		todoService.addTodo((String)modelMap.get("name"), todo.getDescription(), LocalDate.now().plusMonths(11), false);
+		return "redirect:list-todos";
+	}
+	
+	@GetMapping("delete-todo")
+	public String deleteTodo(@RequestParam int id) {
+		System.out.println("deleteTodo_");
+		todoService.deleteById(id);
+		System.out.println("Todo deleted! "+id);
 		return "redirect:list-todos";
 	}
 }
